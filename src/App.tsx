482a3a5c93d420732b7f81cddb8ac8f43f76f2d4
Abduljebar Sani <Abduljebar49@gmail.com>
2025-10-20 19:21:38 +0300

    updated version number

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import { TextEditor } from "./components/TextEditor";
 function App() {
   return (
     <TextEditor
-    height={'min-h-[400px]'}
+      height={"min-h-[400px]"}
       onChange={(content, html, title) => {
         console.log("Content changed:", content);
         console.log("HTML changed:", html);
-        console.log("title : ",title)
+        console.log("title : ", title);
       }}
     />
   );
