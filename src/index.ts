f459c0451801e515ab72d4c34acdbcb4001bdcc6
Abduljebar Sani <Abduljebar49@gmail.com>
2025-10-30 09:45:10 +0300

    yarn build:lib bug fix

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ import './index.css';
 export { TextEditor } from './components/TextEditor';
 // export { useTextEditor } from './hooks/useTextEditor';
 export type { TextEditorProps } from './types/editor';
-export type { EditorContent } from './schemas/editor.schema';
